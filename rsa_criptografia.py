ce61a7cd9eb63efe5b5ec5bc818f36f00edc759b
anapfreitas <anapsantosfreitas@hotmail.com>
2025-05-01 16:56:48 -0300

    correções usuário digita a mensagem

--- rsa_criptografia.py
+++ rsa_criptografia.py
@@ -10,23 +10,34 @@ def cifrar(mensagem: str, chave_publica):
     mensagem_bytes = mensagem.encode()
     mensagem_cifrada = chave_publica.encrypt(
         mensagem_bytes,
-        padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None)
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
     )
     return mensagem_cifrada
 
 def decifrar(mensagem_cifrada: bytes, chave_privada):
     mensagem_decifrada = chave_privada.decrypt(
         mensagem_cifrada,
-        padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None)
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
     )
     return mensagem_decifrada.decode()
 
 if __name__ == "__main__":
     priv, pub = gerar_chaves()
-    mensagem = "Segredo da Ana Paula"
+
+    mensagem = input("Digite a mensagem que deseja cifrar: ")
+
     cifrada = cifrar(mensagem, pub)
     decifrada = decifrar(cifrada, priv)
 
+    print("\n--- Resultado ---")
     print("Mensagem original:", mensagem)
-    print("Mensagem cifrada (em bytes):", cifrada)
+    print("Mensagem cifrada (bytes):", cifrada)
     print("Mensagem decifrada:", decifrada)
